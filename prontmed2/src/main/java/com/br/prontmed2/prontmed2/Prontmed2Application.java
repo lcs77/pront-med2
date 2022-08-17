00a9347467328871ca72bf7fef6e84bddd7b272b
lusca <lcs.job77@gmail.com>
2022-08-17 19:46:29 -0300

    primeiro commit

---
+++ prontmed2/src/main/java/com/br/prontmed2/prontmed2/Prontmed2Application.java
@@ -0,0 +1,13 @@
+package com.br.prontmed2.prontmed2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prontmed2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prontmed2Application.class, args);
+	}
+
+}
---
+++ prontmed2/src/test/java/com/br/prontmed2/prontmed2/Prontmed2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.prontmed2.prontmed2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prontmed2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
