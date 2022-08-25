34579ae70713b8133a9eac855b5da6d3ae957930
Jamille Da silva <silvajamille808@gmail.com>
2022-08-25 19:46:17 -0300

    atualizando AdmRepository

--- prontmed2/src/main/java/com/br/prontmed2/prontmed2/repository/AdmRepository.java
+++ prontmed2/src/main/java/com/br/prontmed2/prontmed2/repository/AdmRepository.java
@@ -1,11 +1,12 @@
 package com.br.prontmed2.prontmed2.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.br.prontmed2.prontmed2.models.AdmModel;
 
 
 
-public interface AdmRepository extends CrudRepository<AdmModel,String> {
-    
+public interface AdmRepository extends JpaRepository<AdmModel,Long>  {
+    //classe que possui um metodo abstrato que salva a entidade Paciente no banco de dados descrito nas propriedades do application.yml
+
 }
