51058467e2298d4f197617fa522e7158abd55a2f
lusca <lcs.job77@gmail.com>
2022-08-24 21:02:39 -0300

    atualização funcional

--- prontmed2/src/main/java/com/br/prontmed2/prontmed2/controllers/AdmController.java
+++ prontmed2/src/main/java/com/br/prontmed2/prontmed2/controllers/AdmController.java
@@ -1,21 +1,40 @@
 package com.br.prontmed2.prontmed2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.br.prontmed2.prontmed2.models.AdmModel;
+import com.br.prontmed2.prontmed2.repository.AdmRepository;
+
 @Controller 
 public class AdmController {
+
+    @Autowired
+    private AdmRepository ar;
 
     @RequestMapping(value = "/inicio", method=RequestMethod.GET)
     public String menu(){
         return "perfil/landingPage";
     }
 
-    @RequestMapping(value = "/login", method=RequestMethod.GET)
+    @RequestMapping(value = "/cadastroAdm", method=RequestMethod.GET)
+    public String cadastro(){
+        return "cadastros/cadastroAdm";
+    }
+    @RequestMapping(value = "/cadastroAdm", method=RequestMethod.POST)
+    public String cadastro(AdmModel admModel){
+        ar.save(admModel);
+        return "cadastros/cadastroAdm";
+    }
+   
+    @RequestMapping( "/login")
     public String login(){
-        return "a"
+        return "logins/loginFuncionario";
+        
+
     }
 
 
